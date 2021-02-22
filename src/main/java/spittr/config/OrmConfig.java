4e019eb4e3dc22b8c96e610dfd8d0da51e0146f7
Alex-3102 <1175106117@qq.com>
2021-02-22 12:37:59 +0800

    增加对JPA的Spring支持

--- src/main/java/spittr/config/OrmConfig.java
+++ src/main/java/spittr/config/OrmConfig.java
@@ -6,8 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -22,32 +28,57 @@ public class OrmConfig {
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
-
+//hibernate和spring集成
+//    @Bean
+//    public LocalSessionFactoryBean sessionFactoryBean(DataSource dataSource){
+//        LocalSessionFactoryBean sfb = new LocalSessionFactoryBean();
+//        sfb.setDataSource(dataSource);
+//        sfb.setPackagesToScan(new String[]{ "spittr"});
+//        Properties props = new Properties();
+//        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+////        props.setProperty("hibernate.current_session_context_class", "thread");
+//        sfb.setHibernateProperties(props);
+//        return sfb;
+//    }
+//
+//    @Bean
+//    public SessionFactory sessionFactory(LocalSessionFactoryBean sfb){
+//        System.out.println(sfb);
+//        return sfb.getObject();
+//    }
+//
+//    @Bean
+//    public PlatformTransactionManager annotationDrivenTransactionManager(SessionFactory sessionFactory) {
+//        System.out.println(sessionFactory);
+//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//        transactionManager.setSessionFactory(sessionFactory);
+//        return transactionManager;
+//    }
+//JPA
+    //JPA适配器
     @Bean
-    public LocalSessionFactoryBean sessionFactoryBean(DataSource dataSource){
-        LocalSessionFactoryBean sfb = new LocalSessionFactoryBean();
-        sfb.setDataSource(dataSource);
-        sfb.setPackagesToScan(new String[]{ "spittr"});
-        Properties props = new Properties();
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-//        props.setProperty("hibernate.current_session_context_class", "thread");
-        sfb.setHibernateProperties(props);
-        return sfb;
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(false);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        return adapter;
     }
 
     @Bean
-    public SessionFactory sessionFactory(LocalSessionFactoryBean sfb){
-        System.out.println(sfb);
-        return sfb.getObject();
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter){
+        LocalContainerEntityManagerFactoryBean emfb = new LocalContainerEntityManagerFactoryBean();
+        emfb.setDataSource(dataSource);
+        emfb.setJpaVendorAdapter(jpaVendorAdapter);
+        emfb.setPackagesToScan("spittr");//识别持久化单元实体类
+        return emfb;
     }
 
     @Bean
-    public PlatformTransactionManager annotationDrivenTransactionManager(SessionFactory sessionFactory) {
-        System.out.println(sessionFactory);
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory);
+    public PlatformTransactionManager annotationDrivenTransactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
         return transactionManager;
     }
-
-
 }
---
+++ src/main/java/spittr/data/JpaSpitterRepository.java
@@ -0,0 +1,37 @@
+package spittr.data;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import spittr.Spitter;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+
+/**
+ * @author arc3102
+ * @date 2021/2/20 23:13
+ */
+@Repository
+@Transactional
+public class JpaSpitterRepository implements SpitterRepository{
+
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public Spitter save(Spitter spitter) {
+        entityManager.persist(spitter);
+        return spitter;
+    }
+
+    @Override
+    public Spitter findByUsername(String username) {
+        return (Spitter) emf.createEntityManager().createQuery("select s from Spitter s where s.username=?").setParameter(1, username).getSingleResult();
+    }
+}
